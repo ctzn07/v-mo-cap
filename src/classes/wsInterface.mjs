8e505aaf58cb3ba4ac3868d0b0f4e8291d18ae1c
ctzn07 <19950634+ctzn07@users.noreply.github.com>
2025-11-02 12:09:15 +0200

    checkpoint

--- src/classes/wsInterface.mjs
+++ src/classes/wsInterface.mjs
@@ -7,44 +7,55 @@ export class WsInterface {
         this.emitter = new wsInterfaceEmitter()
         this.ws = websocket
         this.packetId = 0
-        this.ws.on('message', (data) => {
-            const packet = JSON.parse(data)
-            this.emitter.emit(`${packet.type}-${packet.id}`, packet)
-        })
-    }
-    
-    newId(){
-        this.packetId = this.packetId < 256 ? this.packetId++ : 0 
-        return this.packetId
+        this.ws.on('message', (data, isBinary) => this.receive(data, isBinary))
     }
 
-    send(data, type = 'send', id = 0){
-        const packet = JSON.stringify({
-                type: type, 
-                id: id, 
-                data: data, 
-        })
-        this.ws.send(packet)
+    decode(arr){ return arr.map(n => String.fromCharCode(n)).join('') }
+
+    receive(payload, isBinary){
+        const packet = {}
+        try{
+            if(isBinary){
+                Object.assign(packet, JSON.parse(this.decode(packet)))
+            }
+            else{
+                Object.assign(packet, JSON.parse(payload.data))
+            }
+        }
+        catch(error){ packet.error = error }
+
+        //if packet has id, emit on response channel, else on regular channel
+        if(packet.id){ this.emitter.emit(`response-${packet.id}`, packet) }
+        else{ this.emitter.emit(`receive`, packet) }
+    }
+    
+    updateId(){ this.packetId = this.packetId < 256 ? this.packetId++ : 1 }
+
+    send(data, isRequest = false){
+        const packet = {}
+
+        if(isRequest){ packet.id = this.packetId }
+        packet.data = data
+
+        this.ws.send(JSON.stringify(packet))
     }
 
     request(path){
-        const requestId = this.newId()
-
-        this.send(path, 'request', requestId)
+        updateId()
+        this.send(path, true)
 
         return new Promise((resolve, reject) => {
-            const identifier = `response-${requestId}`
+            const res = `response-${this.packetId}`
             
             const timer = setTimeout(() => {
-                this.emitter.removeAllListeners(identifier)
-                reject()
+                this.emitter.removeAllListeners(res)
+                reject({error: 'Request timed out'})
             }, 3000)
 
-            this.emitter.once(identifier, (data) => {
+            this.emitter.once(res, (packet) => {
                 clearTimeout(timer)
-                resolve(data)
+                resolve(packet)
             })
         })
-    }
-    
+    } 
 }
