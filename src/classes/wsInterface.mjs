e4fb3e45c7a14a044b977cefcd12069a57ffb889
ctzn07 <19950634+ctzn07@users.noreply.github.com>
2025-11-04 13:49:35 +0200

    checkpoint

--- src/classes/wsInterface.mjs
+++ src/classes/wsInterface.mjs
@@ -6,56 +6,70 @@ export class WsInterface {
     constructor(websocket) {
         this.emitter = new wsInterfaceEmitter()
         this.ws = websocket
-        this.packetId = 0
         this.ws.on('message', (data, isBinary) => this.receive(data, isBinary))
+        this.ws.on('error', (e) => this.emitter.emit('error', e))
+        this.ws.on('close', (code, reason) => this.emitter.emit('close', `${code}-${reason}`))
     }
-
+    //client.readyState === WebSocket.OPEN
     decode(arr){ return arr.map(n => String.fromCharCode(n)).join('') }
 
     receive(payload, isBinary){
         const packet = {}
         try{
-            if(isBinary){
-                Object.assign(packet, JSON.parse(this.decode(packet)))
-            }
-            else{
-                Object.assign(packet, JSON.parse(payload.data))
-            }
+            if(isBinary){ Object.assign(packet, JSON.parse(this.decode(packet))) }
+            else{ Object.assign(packet, JSON.parse(payload.data)) }
         }
-        catch(error){ packet.error = error }
+        catch(e){ packet.error = e }
 
-        //if packet has id, emit on response channel, else on regular channel
-        if(packet.id){ this.emitter.emit(`response-${packet.id}`, packet) }
-        else{ this.emitter.emit(`receive`, packet) }
-    }
-    
-    updateId(){ this.packetId = this.packetId < 256 ? this.packetId++ : 1 }
-
-    send(data, isRequest = false){
-        const packet = {}
-
-        if(isRequest){ packet.id = this.packetId }
-        packet.data = data
-
-        this.ws.send(JSON.stringify(packet))
+        this.emitter.emit(`${packet.id}`, packet) 
     }
 
-    request(path){
-        updateId()
-        this.send(path, true)
+    request(path, o_data){
+        //create unique request id
+        const identifier = crypto.randomUUID().split('-').at(-1)
 
-        return new Promise((resolve, reject) => {
-            const res = `response-${this.packetId}`
-            
+        //create packet
+        const packet = {
+            id: identifier, 
+            api: path, 
+            data: o_data || null
+        }
+
+        const promise = new Promise((resolve, reject) => {
             const timer = setTimeout(() => {
-                this.emitter.removeAllListeners(res)
-                reject({error: 'Request timed out'})
+                this.emitter.removeAllListeners(identifier)
+                reject('Request timed out')
             }, 3000)
 
-            this.emitter.once(res, (packet) => {
+            this.emitter.once(identifier, (package) => {
                 clearTimeout(timer)
-                resolve(packet)
+                
+                if(package.error){
+                    //there is an error, reject
+                    reject(package.error)
+                }
+                else{
+                    //otherwise, resolve
+                    resolve(package)
+                }
             })
         })
-    } 
+        
+        if(this.ws && this.ws.readyState === WebSocket.OPEN){
+            //if websocket is open, stringify and send packet
+            try { this.ws.send(JSON.stringify(packet)) }
+            catch(e) {
+                clearTimeout(promise.timer)
+                this.emitter.removeAllListeners(identifier)
+                promise.reject(e)
+            }
+        }
+        else{
+            //else, reject and clean up
+            clearTimeout(promise.timer)
+            this.emitter.removeAllListeners(identifier)
+            promise.reject('WsInterface is not connected')
+        }
+        return promise
+    }
 }
